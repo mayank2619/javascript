23d447ace3afc637722a15a6c2c1613672c428b7
Mayank <mayanksharma2619@gmail.com>
2023-11-19 19:12:38 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+let accountId = 12345;
+const accountName = "Mayank";
+var accountPassword = "abcde";
+accountCity = "Faridabad";
+let accountState;
+//
+accountId = 67890;
+// accountName = "Mayank Sharma";
+accountPassword = "efghi";
+accountCity = "bengaluru";
+//
+console.log(accountId, accountName, accountPassword, accountCity, accountState);
+console.table([
+  accountId,
+  accountName,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
